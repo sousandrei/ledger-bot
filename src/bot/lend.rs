1632976e89e0c6c96c044ee51485a21f50d0bdf7
Victor Canato <vcanato@gmail.com>
2021-04-19 17:55:12 +0100

    Fix Regex and improve rust lint

--- src/bot/lend.rs
+++ src/bot/lend.rs
@@ -1,27 +1,17 @@
-
 use regex::Regex;
 use teloxide::{adaptors::AutoSend, prelude::UpdateWithCx, types::Message, Bot};
 
-pub async fn handler(
-    cx: UpdateWithCx<AutoSend<Bot>, Message>, 
-    input: String,
-) -> Result<(), Error> {
-    
-    let re = Regex::new("(/ˆMe empresta?$/) (\\d+) (/ˆzenys$/)")?;
+pub async fn handler(cx: UpdateWithCx<AutoSend<Bot>, Message>, input: String) -> Result<(), Error> {
+    let re = Regex::new("Me empresta\\? (\\d+) zenys")?;
 
-    let caps = re.captures(&.input);
+    let caps = re.captures(&input);
 
     if caps.is_none() {
-        error!("Not enough parameters: {:?}", input);
-
-        let error = Err(Error::new(
-            "Errou feio, errou rude! Exemplo de uso do comando:\nMe empresta? 10000 zenys",
-        ));
-
-        cx.answer(error.message).await?;
+        cx.answer("Errou feio, errou rude! Exemplo de uso do comando:\nMe empresta? 10000 zenys")
+            .await?;
         return Ok(());
     }
 
     cx.answer("Não...").await?;
-    return Ok(());
+    Ok(())
 }
